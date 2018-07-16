d434f771f39e14339a786107fb83626fc1796329
Timur_hnimdvi <timurbakibayev@gmail.com>
2018-07-16 11:25:24 +0600

    first working version

--- computability/comp/admin.py
+++ computability/comp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from comp.models import *
 
+admin.site.register(Post)
+admin.site.register(Comment)
 # Register your models here.
--- computability/computability/urls.py
+++ computability/computability/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
+from comp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.index),
+    url(r'^admin/', admin.site.urls),
 ]
